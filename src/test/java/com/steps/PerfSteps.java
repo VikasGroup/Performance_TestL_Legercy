712d5fbf24b8e170bd581c626b63c2dfcbbd2733
Pavithri <Pavithri@Pavithri-PC>
2017-06-06 11:50:25 +0530

    in commit

--- src/test/java/com/steps/PerfSteps.java
+++ src/test/java/com/steps/PerfSteps.java
@@ -1,36 +1,39 @@
 package com.steps;
 
+import java.util.ArrayList;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import vikas.WPDashboardPgObj;
-import vikas.WPHomePgObj;
-import vikas.WPLoginPgObj;
+import vikas.INDashboardPgObj;
+import vikas.INHomePgObj;
+import vikas.INLoginPgObj;
+
 
 public class PerfSteps {
 	
 	public static WebDriver driver = new FirefoxDriver();;
-	WPHomePgObj wpHomePgObj;
-	WPLoginPgObj wpLoginPgObj;
-	WPDashboardPgObj wpDashboardPgObj;
+	INHomePgObj inHomePgObj;
+	INLoginPgObj inLoginPgObj;
+	INDashboardPgObj inDashboardPgObj;
 
 
 	@Given("^user direct to the site$")
 	public void user_direct_to_the_site() throws Throwable {
 		//driver = new FirefoxDriver();
 		driver.manage().window().maximize();
-		driver.get("http://www.qnet.net/");
+		driver.get("http://www.qnetindia.in/");
 		Thread.sleep(5000);
 	 
 	}
 
 	@When("^user click on Login button$")
 	public void user_click_on_Login_button() throws Throwable {
-		 wpHomePgObj= new WPHomePgObj();
-	    wpHomePgObj.click_irlogin();
+		inHomePgObj = new INHomePgObj();
+		inHomePgObj.click_irlogin();
 	 
 	}
 	
@@ -41,35 +44,38 @@ public class PerfSteps {
 
 	@When("^user enter a valid ID \"([^\"]*)\"$")
 	public void user_enter_a_valid_ID(String arg1) throws Throwable {
-		wpLoginPgObj = new WPLoginPgObj();
-	    wpLoginPgObj.enter_irid(arg1);
+		inLoginPgObj = new INLoginPgObj();
+		inLoginPgObj.enter_irid(arg1);
 	 
 	}
 
 	@When("^user enter a password \"([^\"]*)\"$")
 	public void user_enter_a_password(String arg1) throws Throwable {
-	    wpLoginPgObj.enter_pwd(arg1);
+	    inLoginPgObj.enter_pwd(arg1);
 	
 	}
 
 	@Then("^user click on login button$")
 	public void user_click_on_login_button() throws Throwable {
-	    wpLoginPgObj.click_login();
+	  inLoginPgObj.click_login();
 	 
 	}
 
 	@Then("^validate the landing page \"([^\"]*)\"$")
 	public void validate_the_landing_page(String arg1) throws Throwable {
-	    
+		ArrayList<String> tabs2 = new ArrayList<String>(driver.getWindowHandles());
+		  driver.switchTo().window(tabs2.get(1));
+		  driver.switchTo().window(tabs2.get(2));
+		  driver.switchTo().window(tabs2.get(2)).close();
+		  driver.switchTo().window(tabs2.get(1)).close();
 	 
 	}
 
 	@Then("^user click on shop link$")
 	public void user_click_on_shop_link() throws Throwable {
-		wpDashboardPgObj = new WPDashboardPgObj();
-		wpDashboardPgObj.click_close();
-	    wpDashboardPgObj.click_shop();
-	    driver.close();
+		inDashboardPgObj = new INDashboardPgObj();
+		inDashboardPgObj.click_shop();
+	   
 	}
 	
 }
